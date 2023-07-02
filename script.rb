039a32b5eb3da1b00d626e62dcb3de71baa85a5b
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-07-02 18:30:48 +0300

    Remove newline characters from array items

--- script.rb
+++ script.rb
@@ -1,6 +1,6 @@
 contents = File.read('google-10000-english-no-swears.txt')
-
+  
 lines = File.readlines('google-10000-english-no-swears.txt')
-lines.each do |line|
-  puts line
-end
+new_lines = lines.map {|line| line.gsub(/[\s]/, '')}
+
+
