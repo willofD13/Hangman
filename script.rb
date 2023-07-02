0c446bba9e1ef07e301b137c368b78a69bf30e15
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-07-02 23:23:34 +0300

    Complete guess counting and display

--- script.rb
+++ script.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-
+require 'pry-byebug'
 class Game
   @@guesses = 6 ##keep count with this
 
@@ -11,8 +11,7 @@ class Game
   def initialize
     @lines = File.readlines('google-10000-english-no-swears.txt').map { |line| line.gsub(/\s/, '') }
     
-    @correct_letters = []
-    @incorrect_letters = []
+    
     @words = []
     @lines.each do |line|
       @words.push(line) if line.length > 5 && line.length < 12
@@ -21,29 +20,38 @@ class Game
     @secret_word = @words.sample.split('')
     @display = Array.new(@secret_word.length, '_')
     puts @display.join(' ')
+    make_guess(secret_word,display)
   end
 
 
   def make_guess(secret_word,display)
+    correct_letters = []
+    incorrect_letters = []
     while @@guesses > 0
-      puts 'Make your guess'
+      puts "Make your guess. #{@@guesses}guesses and you are hanged!"
       letter = gets.chomp
-    
+      
  
-    if secret_word.include?(letter) == true
-      correct_letters.push(letter)
-    else
-      incorrect_letters.push(letter)
-    end
-    puts correct_letters
-    puts incorrect_letters
+      if secret_word.include?(letter) == true
+        correct_letters.push(letter)
+      else
+        incorrect_letters.push(letter)
+        @@guesses -= 1
+      end
+      puts "correct letters :#{correct_letters}"
+      puts "incorrect letters : #{incorrect_letters}"
 
-    i = 0
-    while i < secret_word.length
-      display[i] = letter if secret_word[i].eql?(letter) == true
-      i += 1
+      i = 0
+      while i < secret_word.length
+        display[i] = letter if secret_word[i].eql?(letter) == true
+        i += 1
+      end
+      puts display.join(' ')
+      
+      #binding.pry
     end
   end
 end
 
 game = Game.new
+
