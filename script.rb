43a63b9cede5095a7255d4a30bfff9e464e8db13
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-07-02 18:50:23 +0300

    Select a word for the game

--- script.rb
+++ script.rb
@@ -5,10 +5,10 @@ File.read('google-10000-english-no-swears.txt')
 lines = File.readlines('google-10000-english-no-swears.txt')
 new_lines = lines.map { |line| line.gsub(/\s/, '') }
 
-def select_word
+def select_word(new_lines)
   words = []
   new_lines.each do |line|
     words.push(line) if line.length > 5 && line.length < 12
   end
-  words.sample
+  secret_word = words.sample
 end
