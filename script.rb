449b5b389ef8f324a6131eade3303f1aece4de9a
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-07-02 22:39:12 +0300

    Create game class and initialize instance variables

--- script.rb
+++ script.rb
@@ -1,44 +1,42 @@
 # frozen_string_literal: true
 
-File.read('google-10000-english-no-swears.txt')
+class Game
+  File.read('google-10000-english-no-swears.txt')
+  
+  def initialize
+    @lines = File.readlines('google-10000-english-no-swears.txt').map { |line| line.gsub(/\s/, '') }
 
-lines = File.readlines('google-10000-english-no-swears.txt')
-new_lines = lines.map { |line| line.gsub(/\s/, '') }
+    @words = []
+    @lines.each do |line|
+      @words.push(line) if line.length > 5 && line.length < 12
+    end
 
-def select_word(new_lines)
-  words = []
-  new_lines.each do |line|
-    words.push(line) if line.length > 5 && line.length < 12
+    @secret_word = @words.sample.split('')
+    @display = Array.new(@secret_word.length, '_')
+    puts @display.join(' ')
   end
-  p secret_word = words.sample.split('')
-  display = Array.new(secret_word.length, '_')
-  puts display.join(' ')
-  make_guess(secret_word,display)
-end
 
-def make_guess (secret_word,display)
-puts "Make your guess"
-letter = gets.chomp
 
-correct_letters = []
-incorrect_letters = []
-
-def display_choices
-  if secret_word.include?(letter) == true
-    correct_letters.push(letter)
-  else 
-    incorrect_letters.push(letter)
+  def make_guess(_secret_word, _display)
+    puts 'Make your guess'
+    gets.chomp
   end
-  puts correct_letters
-  puts incorrect_letters
 
-  i = 0
-  while i < secret_word.length
-    if secret_word[i].eql?(letter) == true
-      display[i] = letter
-    end 
-    i += 1
+  def display_choices
+    if secret_word.include?(letter) == true
+      correct_letters.push(letter)
+    else
+      incorrect_letters.push(letter)
+    end
+    puts correct_letters
+    puts incorrect_letters
+
+    i = 0
+    while i < secret_word.length
+      display[i] = letter if secret_word[i].eql?(letter) == true
+      i += 1
+    end
   end
 end
 
-select_word(new_lines)
+game = Game.new
