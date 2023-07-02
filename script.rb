fe001e78a8d9132ecc6a2016a87ac6718a12281f
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-07-02 18:37:15 +0300

    Select a word for the game

--- script.rb
+++ script.rb
@@ -3,4 +3,10 @@ contents = File.read('google-10000-english-no-swears.txt')
 lines = File.readlines('google-10000-english-no-swears.txt')
 new_lines = lines.map {|line| line.gsub(/[\s]/, '')}
 
+words = []
+new_lines.each do |line|
+  if line.length > 5 && line.length < 12 
+    words.push(line)
+  end 
+end 
 
