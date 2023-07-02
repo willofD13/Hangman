679b53befc2e9a0ddcfa037d6e7b492561adc9f1
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-07-02 22:26:25 +0300

    Display chosen letters

--- script.rb
+++ script.rb
@@ -10,20 +10,34 @@ def select_word(new_lines)
   new_lines.each do |line|
     words.push(line) if line.length > 5 && line.length < 12
   end
-  secret_word = words.sample.split('')
+  p secret_word = words.sample.split('')
   display = Array.new(secret_word.length, '_')
   puts display.join(' ')
+  make_guess(secret_word,display)
 end
-select_word(new_lines)
 
+def make_guess (secret_word,display)
 puts "Make your guess"
 letter = gets.chomp
 
 correct_letters = []
 incorrect_letters = []
 
+def display_choices
+  if secret_word.include?(letter) == true
+    correct_letters.push(letter)
+  else 
+    incorrect_letters.push(letter)
+  end
+end
+
 i = 0
 while i < secret_word.length
-  display[i] = letter if secret_word[i]eql?(letter)
+  if secret_word[i].eql?(letter) == true
+    display[i] = letter
+  end 
   i += 1
 end
+end
+
+select_word(new_lines)
